5c4101042ff915c5e33c9599bdaad5033800b8f6
Chris Mague <github@mague.com>
2021-12-03 12:31:54 -0800

    grab results

--- streams-to-ws.go
+++ streams-to-ws.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -9,10 +10,14 @@ import (
 
 	"html/template"
 
+	"context"
+
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/websocket"
 )
 
 var (
+	ctx      = context.Background()
 	addr     = flag.String("addr", ":8080", "http service address")
 	upgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
@@ -28,15 +33,25 @@ const (
 	writeWait = 10 * time.Second
 )
 
-func readStream(lastMod time.Time) ([]byte, time.Time, error) {
+type Streams struct {
+	rdb *redis.Client
+}
+
+func readStream(lastMod time.Time, rdb *redis.Client) ([]byte, time.Time, error) {
 	var p []byte
-	p = []byte(strconv.FormatInt(lastMod.UnixMilli(), 10))
-	log.Println("readStream:", strconv.FormatInt(lastMod.UnixMilli(), 10))
+	//p = []byte(strconv.FormatInt(lastMod.UnixMilli(), 10))
+	r, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Println(err)
+		return []byte(r), time.Now(), err
+	}
+
+	p = []byte(strconv.FormatInt(lastMod.UnixMilli(), 10) + " " + r)
 
 	return p, time.Now(), nil
 }
 
-func writer(ws *websocket.Conn, lastMod time.Time) {
+func writer(ws *websocket.Conn, lastMod time.Time, rdb *redis.Client) {
 	lastError := ""
 	fileTicker := time.NewTicker(filePeriod)
 	defer func() {
@@ -49,7 +64,7 @@ func writer(ws *websocket.Conn, lastMod time.Time) {
 			var p []byte
 			var err error
 
-			p, lastMod, err = readStream(lastMod)
+			p, lastMod, err = readStream(lastMod, rdb)
 
 			if err != nil {
 				if s := err.Error(); s != lastError {
@@ -70,7 +85,7 @@ func writer(ws *websocket.Conn, lastMod time.Time) {
 	}
 }
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
+func (stream *Streams) serveWs(w http.ResponseWriter, r *http.Request) {
 	var lastMod time.Time
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -84,16 +99,16 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		lastMod = time.Unix(0, n)
 	}
 
-	go writer(ws, lastMod)
+	go writer(ws, lastMod, stream.rdb)
 }
 
-func serveTest(w http.ResponseWriter, r *http.Request) {
+func (stream *Streams) serveTest(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	p, lastMod, err := readStream(time.Unix(0, 0))
+	p, lastMod, err := readStream(time.Unix(0, 0), stream.rdb)
 	if err != nil {
 		p = []byte(err.Error())
 		lastMod = time.Unix(0, 0)
@@ -111,8 +126,20 @@ func serveTest(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/ws", serveWs)
-	http.HandleFunc("/test", serveTest)
+	redisClient := redis.NewClient(&redis.Options{
+		Password:        "",
+		Addr:            fmt.Sprintf("%s:%d", "localhost", 6379),
+		DB:              0,
+		MinIdleConns:    1,                    // make sure there are at least this many connections
+		MinRetryBackoff: 8 * time.Millisecond, //minimum amount of time to try and backupf
+		MaxRetryBackoff: 5000 * time.Millisecond,
+		MaxConnAge:      0,  //3 * time.Second this will cause everyone to reconnect every 3 seconds - 0 is keep open forever
+		MaxRetries:      10, // retry 10 times : automatic reconnect if a proxy is killed
+		IdleTimeout:     time.Second,
+	})
+	streams := &Streams{rdb: redisClient}
+	http.HandleFunc("/ws", streams.serveWs)
+	http.HandleFunc("/test", streams.serveTest)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal(err)
 	}
